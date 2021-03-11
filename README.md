39982e5a6debdc7ea3b0805a793636652c6ec9a0
Hadar Dahan <hadar.dahan@mail.huji.ac.il>
2021-03-11 16:13:41 +0200

    added GIT2
    some description

---
+++ README.md
@@ -0,0 +1,3 @@
+# Demo 2.0
+
+some text!
---
+++ cmake-build-debug/CMakeFiles/GIT2.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/GIT2.dir/main.cpp.o"
+  "GIT2.exe"
+  "GIT2.pdb"
+  "libGIT2.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/GIT2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
